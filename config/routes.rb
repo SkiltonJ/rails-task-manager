1df4e684db48f2847d3ad0d10f26c51e9c953264
Jackson Jaensch <jackson.jaensch14@gmail.com>
2022-11-08 15:30:30 +1030

    Add show method to controller and create show html page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  root "tasks#index"
+  get '/tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: :show
 end
